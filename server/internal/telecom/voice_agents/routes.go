8340b6c58ce0bb12f4009f057ad7281d3c17f2f8
Coffey Vidzro <coffeyvidzro@gmail.com>
2026-09-26 12:13:49 +0000

    feat(voice): add voice agent HTTP domain

---
+++ server/internal/telecom/voice_agents/routes.go
@@ -0,0 +1,22 @@
+package voice_agents
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func RegisterRoutes(router chi.Router, handler *Handler, auth func(http.Handler) http.Handler) {
+	router.Route("/voice-agents", func(r chi.Router) {
+		r.Use(auth)
+		r.Post("/", handler.Create)
+		r.Get("/", handler.List)
+		r.Get("/{voice_agent_id}", handler.Get)
+		r.Patch("/{voice_agent_id}", handler.Update)
+		r.Delete("/{voice_agent_id}", handler.Delete)
+
+		r.Post("/{voice_agent_id}/bindings", handler.CreateBinding)
+		r.Get("/{voice_agent_id}/bindings", handler.ListBindings)
+		r.Delete("/{voice_agent_id}/bindings/{binding_id}", handler.DeleteBinding)
+	})
+}
